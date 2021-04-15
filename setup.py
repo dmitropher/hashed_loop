80d0a185aeea07fcfd47ef0a69838e689f4badc2
dzorine <d.zorine@gmail.com>
2021-04-15 15:23:56 -0700

    added some packaging stuff

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="hashed_loop",
+    version="0.1",
+    description="Loop closure for use with Rosetta built with xbin and getpy",
+    author="Dmitri Zorine",
+    author_email="d.zorine@gmail.com",
+    license="MIT",
+    packages=["hashed_loop"],
+    zip_safe=False,
+)
