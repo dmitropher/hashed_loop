6ebc3643e4ed719fac9e146b9150c16f69a8d2dc
dzorine <dzorine@gmail.com>
2021-04-30 15:39:25 -0700

    more rebuilding non-lfs nodes

--- setup.py
+++ setup.py
@@ -1,3 +1,29 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:87c3a524aab09e78f441f0b1965ac24b7faedd58c0d5e492b92df16912f49182
-size 799
+# import setuptools
+from setuptools import setup
+
+# import distutils.command.build
+# from setuptools.command.install import install
+
+# copy store imports
+# import os
+# from shutil import copyfile
+
+
+setup(
+    name="hashed_loop",
+    version="0.1",
+    description="Loop closure for use with Rosetta built with xbin and getpy",
+    author="Dmitri Zorine",
+    author_email="d.zorine@gmail.com",
+    license="MIT",
+    packages=["hashed_loop"],
+    zip_safe=False,
+    install_requires=["Click", "xbin", "getpy"],
+    entry_points={
+        "console_scripts": [
+            "import_default_loop_table = hashed_loop.import_default_loop_table:main",
+            "close_loop = hashed_loop.close_loop:main",
+            "build_hash_loop_table = hashed_loop.build_hash_loop_table:main",
+        ]
+    },
+)
