b72f8ac98a6d821783a7559706df2ecb462e6d5e
dzorine <dzorine@gmail.com>
2021-04-30 16:42:16 -0700

    adding silent to default func

--- hashed_loop/import_default_loop_table.py
+++ hashed_loop/import_default_loop_table.py
@@ -5,11 +5,18 @@ from shutil import copyfile
 
 @click.command()
 @click.argument("store_path")
-def main(store_path):
+@click.argument("silent_path")
+def main(store_path, silent_path):
     """
     """
-    dest_path = os.path.join(
+    dest_path_store = os.path.join(
         os.path.dirname(__file__), "resources/hdf5_archives/default.hf5"
     )
-    os.makedirs(os.path.dirname(dest_path), exist_ok=True)
-    copyfile(store_path, dest_path)
+    os.makedirs(os.path.dirname(dest_path_store), exist_ok=True)
+    copyfile(store_path, dest_path_store)
+
+    dest_path_silent = os.path.join(
+        os.path.dirname(__file__), "resources/silent_files/default.silent"
+    )
+    os.makedirs(os.path.dirname(dest_path_silent), exist_ok=True)
+    copyfile(silent_path, dest_path_silent)
