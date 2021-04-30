0204fa84bce1bde53972b61392e736f9b08e5da5
dzorine <dzorine@gmail.com>
2021-04-30 14:43:21 -0700

    changed to entry points

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ab8de578d3b0227c411a8a70b8313765437498bd88dec22e3e505bcec43b34d7
-size 790
+oid sha256:5b7e4a58d7f798486bb620436088294b9c62bb532717ce22b8eba4971afe2032
+size 796
