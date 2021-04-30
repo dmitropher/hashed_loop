ef18dbf48f8ff4a2dc2f79808def390a7b11dae9
dzorine <dzorine@gmail.com>
2021-04-30 13:44:22 -0700

    fixing install stuff

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a2dd7f1d2908a7c04dc4550eca5fcb162efde69769bd051d8605c0060128acdb
-size 1465
+oid sha256:6bcb235cd8825273a64f422c055372bd1d29faf41b538f739ec0faeb22fbe815
+size 1516
