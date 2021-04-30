6898077c5322c1bf1e0776af266a71da41681c5f
dzorine <dzorine@gmail.com>
2021-04-30 14:45:57 -0700

    I am once again asking for your minor change

--- hashed_loop/import_default_loop_table.py
+++ hashed_loop/import_default_loop_table.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a31e2686d668674126fb4970150a4cd007fb77f95036448d674710be35ae9f0e
-size 271
+oid sha256:bfbaec6f6d905a26143e0b786f22b2771bd6c02be3ba2778eb77a5fc284e3d95
+size 288
