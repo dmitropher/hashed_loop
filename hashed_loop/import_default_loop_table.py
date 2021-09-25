d2ce3b02ccfc182babc6d39c6f1a49a7e653781e
dmitropher <dzorine@gmail.com>
2021-09-24 23:04:20 -0700

    added link mode and maybe fixed imports

--- hashed_loop/import_default_loop_table.py
+++ hashed_loop/import_default_loop_table.py
@@ -1,22 +1,32 @@
 import click
 import os
-from shutil import copyfile
+from shutil import copyfile, move
 
 # TODO Add link mode to this script so you don't ahve to copy resources every time
 @click.command()
 @click.argument("store_path")
 @click.argument("silent_path")
-def main(store_path, silent_path):
+def main(store_path, silent_path, link_mode=False):
     """
+    Register the silent and data table, link_mode makes symlinks instead of copy
+
     """
     dest_path_store = os.path.join(
         os.path.dirname(__file__), "resources/hdf5_archives/default.hf5"
     )
-    os.makedirs(os.path.dirname(dest_path_store), exist_ok=True)
-    copyfile(store_path, dest_path_store)
 
     dest_path_silent = os.path.join(
         os.path.dirname(__file__), "resources/silent_files/default.silent"
     )
+    os.makedirs(os.path.dirname(dest_path_store), exist_ok=True)
     os.makedirs(os.path.dirname(dest_path_silent), exist_ok=True)
-    copyfile(silent_path, dest_path_silent)
+    if link_mode:
+        if os.path.isfile(dest_path_store):
+            move(dest_path_store + ".old")
+        if os.path.isfile(dest_path_silent):
+            move(dest_path_silent + ".old")
+        os.symlink(store_path, dest_path_store)
+        os.symlink(silent_path, dest_path_silent)
+    else:
+        copyfile(store_path, dest_path_store)
+        copyfile(silent_path, dest_path_silent)
